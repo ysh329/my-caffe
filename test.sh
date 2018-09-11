4c6efe619c2a0990fb6e957b2b802818beaf0187
Yuan Shuai <ysh329@sina.com>
2018-09-11 12:15:42 +0000

    first commit

---
+++ test.sh
@@ -0,0 +1,6 @@
+/opt/caffe/build/examples/cpp_classification/classification.bin \
+./mobilenetv2.prototxt \
+./mobilenetv2.caffemodel \
+/opt/caffe/data/ilsvrc12/imagenet_mean.binaryproto \
+/opt/caffe/data/ilsvrc12/synset_words.txt \
+/home/yuanshuai/code/inferx_model/test/cat.png
